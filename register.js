a3ee8f4bc78b32f043d1099dede5e5d47bbca935
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-04-20 19:52:51 +0200

    Update register.js

--- register.js
+++ register.js
@@ -40,8 +40,8 @@ var applicant = false;
 
 function registerUser(){
     //sign-in using small window prompt
-    signInWithPopup(auth, provider)
     console.log('Hello1');
+    signInWithPopup(auth, provider)
     .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
