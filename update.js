2df67c30a75757e402aeea68972c18561e25c615
DanielDanzo <sempapadaniel123@gmail.com>
2024-05-14 01:15:26 +0200

    test again30

--- update.js
+++ update.js
@@ -1,8 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js"
+import { getFirestore, collection, addDoc, getDocs, doc, query, where, orderBy, updateDoc, or, deleteDoc  } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpsbqDksFVO0JpBqZT4gUGa-qW5PDIyVU",
+    authDomain: "funding-requests-management.firebaseapp.com",
+    databaseURL: "https://funding-requests-management-default-rtdb.firebaseio.com",
+    projectId: "funding-requests-management",
+    storageBucket: "funding-requests-management.appspot.com",
+    messagingSenderId: "663669566432",
+    appId: "1:663669566432:web:d34a19ea3989a6c3ce5985",
+    measurementId: "G-YW4KG1DXWX"
+  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
 let budget;
 let information;
 let dataList;
 let applicantDataList;
 let selectedValue;
+
+
+async function fundingInfo(){
+    const querySnapshot = await getDocs(collection(db, "users"));
+    users = [];
+    querySnapshot.forEach((doc) => {
+      users.push(doc.data());
+    });
+    allInfo.textContent = "Operation Sucessful";
+}
 
 function bursaryInfo(selectedValue, data) {
     for (let i = 0; i < data.length; i++) {
@@ -55,10 +85,23 @@ const url = (input) => {
     return input.split(" ").join("%20");
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+
+async function fundingDropDown(){
+    const querySnapshot = await getDocs(collection(db, "Funding Opportunities"));
+    dropdown.innerHTML = `<option value="Select">Select</option>`;
+    querySnapshot.forEach((doc) => {
+        dropdown.innerHTML += `<option value="${doc.Name}">${doc.Name}</option>`
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
     const dropdown = document.getElementById("funds");
     let updateFunds = document.getElementById("updateFund");
 
+
+    await fundingDropDown();
+
+    /*
     fetch(`https://funding-requests-management-dfae31570a7e.herokuapp.com/funds`, {
         method: 'GET'
     })
@@ -71,7 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }).catch(error => {
         console.error(error);
-    })
+    })*/
 
     dropdown.addEventListener('change', () => {
         selectedValue = dropdown.value;
