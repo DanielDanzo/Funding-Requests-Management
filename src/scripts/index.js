7bb5aaeefa8f6a3763d01c788d8224b371b3468c
PravirPadayachee <160501638+PravirPadayachee@users.noreply.github.com>
2024-05-18 22:07:45 +0200

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,9 +5,7 @@ import { auth } from "../modules/init.js";
 
 //Below we we initialise any variable we might need for our website
 var btn_register = document.getElementById('register-link');
-const btn_applicant_login = document.getElementById('btn-applicant-login');
-const btn_fundManganer_login = document.getElementById('btn-fundManager-login');
-const btn_platformAdmin_login = document.getElementById('btn-platformAdmin-login');
+const btn_login = document.getElementById('btn-login');
 
 var admin = false;
 var fundManager = false;
@@ -22,28 +20,12 @@ btn_register.addEventListener('click',()=>{
     window.location.href = 'https://ambitious-glacier-0cd46151e.5.azurestaticapps.net/register.html';
 });
 
-btn_applicant_login.addEventListener('click',()=>{
+btn_login.addEventListener('click',()=>{
     //After user clicks login. user will be signed in
-    applicant = true;
-    fundManager = false;
-    admin = false;
-    signIn(admin, fundManager, applicant);
-});
-
-btn_fundManganer_login.addEventListener('click',()=>{
-    //After user clicks login. user will be signed in
-    applicant = false;
-    fundManager = true;
-    admin = false;
-    signIn(admin, fundManager, applicant);
-});
-
-btn_platformAdmin_login.addEventListener('click',()=>{
-    //After user clicks login. user will be signed in
-    applicant = false;
-    fundManager = false;
-    admin = true;
-    signIn(admin, fundManager, applicant);
+    //applicant = true;
+   // fundManager = false;
+    //admin = false;
+    //signIn(admin, fundManager, applicant);
 });
 
 
