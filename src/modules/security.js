1b21b52323d2e94ab7d98cf0f8efb83e23ab3f35
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-05-25 16:33:15 +0200

    Update security.js

--- src/modules/security.js
+++ src/modules/security.js
@@ -10,10 +10,10 @@ async function isAuthorised(email, role){
     if(!verified){
         const tempRole = await getUser(email).Role;
         console.log(await getUser(email));
-        if( tempRole == 'Admin'){
+        if( tempRole == "Admin"){
             window.localStorage.setItem("Blocked", role);
             window.location.href = 'https://ambitious-glacier-0cd46151e.5.azurestaticapps.net/AdminUpdate.html'; 
-        }else if( tempRole == 'Fund Manager'){
+        }else if( tempRole == "Fund Manager"){
             window.localStorage.setItem("Blocked", role);
             window.location.href = 'https://ambitious-glacier-0cd46151e.5.azurestaticapps.net/fundmanager.html'; 
         }else{
